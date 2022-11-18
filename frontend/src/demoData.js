74246b3ea3f64fda6ff2f52fcbc5c63b0048f457
Anika Afroz <seikhirfan2011@gmail.com>
2022-11-18 16:02:20 +0000

    Added the timer for the quiz and changed the color of the option on being selected

---
+++ frontend/src/demoData.js
@@ -0,0 +1,46 @@
+const data = {
+    questions: [
+        {
+            "question": "What is the capital of India ?",
+            "answer" : "New Delhi",
+            "option1" : "New Delhi",
+            "option2" : "Mumbai",
+            "option3" : "Kolkata",
+            "option4" : "Hyderabad"
+        },
+        {
+            "question": "Which of these U.S. states does NOT border Canada?",
+            "answer" : "Minnesota",
+            "option1" : "Maine",
+            "option2" : "Alaska",
+            "option3" : "Minnesota",
+            "option4" : "Indiana"
+        },
+        {
+            "question": "Which of these countries was NOT a part of the Soviet Union?",
+            "answer" : "Georgia",
+            "option1" : "Belarus",
+            "option2" : "Ukraine",
+            "option3" : "Georgia",
+            "option4" : "Poland"
+        },
+        {
+            "question": "Which of these cities is NOT a national capital?",
+            "answer" : "Sydney",
+            "option1" : "Prague",
+            "option2" : "Bangkok",
+            "option3" : "Cairo",
+            "option4" : "Sydney"
+        },
+        {
+            "question": "Which of these countries was NEVER part of the British Empire?",
+            "answer" : "Thailand",
+            "option1" : "Ireland",
+            "option2" : "New Zealand",
+            "option3" : "Kenya",
+            "option4" : "Thailand"
+        }
+    ]
+}
+
+export default data;
