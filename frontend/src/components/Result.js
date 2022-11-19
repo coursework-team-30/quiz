5afa063c309fbd73712d2e7f75b96c8e2939d7e7
Irfan <seikhirfan2011@gmail.com>
2022-11-19 23:45:21 +0000

    Changed the sequence of word display

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -10,8 +10,8 @@ const Result = ({score,timer}) => {
   return (
     <div style={{marginTop:"10em",display:"flex",flexDirection:"column", alignItems:"center"}}>
         <h1>{!timer ? "TIME OVER":""}</h1>
+        <h1>Your Score is <span style={{fontSize:"2rem", marginLeft:"10px"}}>{score}/5</span></h1>   
         <h1>{score===5 ? "Perfect Score! Well Done." : (score >=3 && score < 5) ? "Good try! Try Again." : "That's a bad score, try again!"}</h1>
-        <h2>Your Score is <span style={{fontSize:"2rem", marginLeft:"10px"}}>{score}/5</span></h2>   
         <button onClick={refreshPage} className='next-button'>PLAY AGAIN</button> 
     </div>
   )
