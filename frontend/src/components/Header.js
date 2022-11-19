6973d70f783e458e166c8515fdd5554947766ddf
Irfan <seikhirfan2011@gmail.com>
2022-11-19 21:23:45 +0000

    Changed the css to fix some UI issues

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
    <div className='header'>
-        <h2>Quiz Challenges</h2>
+        <h2 className='headTitle'>Quiz Challenges</h2>
         <p className='subHeader'>Take the test to know your worth.</p>
    </div>
   )
