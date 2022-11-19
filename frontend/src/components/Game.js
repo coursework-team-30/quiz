cfa0cff5a5262150ab5ea20063837d010b7ddf79
Anika Afroz <seikhirfan2011@gmail.com>
2022-11-19 23:23:43 +0000

    Added result page

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Header from './Header'
 import './Game.css'
 import data from '../demoData'
+import Result from './Result'
 
 const Game = () => {
 
@@ -10,16 +11,21 @@ const Game = () => {
   const [questionName,setQuestion] = useState(currentQuestion.question);
   const [answer,setAnswer] = useState();
   const [countQuestion,setCount] = useState(1);
-  const [timer,setTimer] = useState(60);
+  const [timer,setTimer] = useState(30);
   const [options,setOptions] = useState([currentQuestion.option1,currentQuestion.option2,currentQuestion.option3,currentQuestion.option4])
   const [correctAnswer,setCorrectAnswer] = useState(data.questions[randomElement].answer);
   const [score,setScore] = useState(0);
+  const [classCorrectAnswer, setClassCorrectAnswer] = useState("");
+  const [isQuizFinished,setIsQuizFinished] = useState(false);
 
 
   useEffect(() => {
     if(timer > 0){
       setTimeout(()=> setTimer(timer - 1),1000);
     }
+
+    if(timer===0)
+      setIsQuizFinished(() => true)
   })
 
 
@@ -27,9 +33,13 @@ const Game = () => {
     setAnswer(() => e.target.value)
     if(e.target.innerText === correctAnswer)
     {
+      // e.target.style.backgroundColor = "green";
+      setClassCorrectAnswer(() => "greenBG");
+      console.log(classCorrectAnswer)
       setScore((prev) => prev + 1);
     }
   }
+
 
   const nextQuestion = () => {
     if(answer){
@@ -38,42 +48,43 @@ const Game = () => {
     setCorrectAnswer(() => data.questions[randomElement].answer)
     setCount((prev) => prev + 1)
     setAnswer(() => "")
-    
+    setClassCorrectAnswer(() => "")
    }
   }
 
 
   const submitAnswers = (e) => {
     if(countQuestion>4){
-      e.target.innerText = "Submit"
+      setIsQuizFinished(()=> true) 
       console.log(score);
     }else{
+      //let correctOption = document.querySelector('button').value(answer);
+      //correctOption.style.backgroundColor = "green"
       nextQuestion()
     }
   }
-
-
-
 
 
   return (
    
     <div className='game'>
         <Header/>
-        <div className='container'>
-        <h2 className='question'>Question {countQuestion}/5</h2>
+        {!isQuizFinished? 
+        (<div className='container'>
+          <h2 className='question'>Question {countQuestion}/5</h2>
         <h3>Time Left: {timer}</h3>
         
         <p className='sub-question'>{questionName}</p>
         <div>
-            <button style={{backgroundColor: `${answer}`=== 'a'? 'rgb(4, 113, 135)':'grey' }} onClick={(e) => selectAnswer(e)} className='but' value={"a"}>{options[0]}</button>
-            <button style={{backgroundColor: `${answer}`=== 'b'? 'rgb(4, 113, 135)':'grey' }} onClick={(e) => selectAnswer(e)} className='but' value={"b"}>{options[1]}</button>
-            <button style={{backgroundColor: `${answer}`=== 'c'? 'rgb(4, 113, 135)':'grey' }} onClick={(e) => selectAnswer(e)} className='but' value={"c"}>{options[2]}</button>
-            <button style={{backgroundColor: `${answer}`=== 'd'? 'rgb(4, 113, 135)':'grey' }} onClick={(e) => selectAnswer(e)} className='but' value={"d"}>{options[3]}</button>
+            <button  onClick={(e) => selectAnswer(e)} className={`but ${answer === 'a'?"hoverOption": "grey"}`}  value={"a"}>{options[0]}</button>
+            <button  onClick={(e) => selectAnswer(e)} className={`but ${answer === 'b'?"hoverOption": "grey"}`} value={"b"}>{options[1]}</button>
+            <button  onClick={(e) => selectAnswer(e)} className={`but ${answer === 'c'?"hoverOption": "grey"}`} value={"c"}>{options[2]}</button>
+            <button  onClick={(e) => selectAnswer(e)} className={`but ${answer === 'd'?"hoverOption": "grey"}`} value={"d"}>{options[3]}</button>
       </div>
-        <button onClick={submitAnswers} className='next-button'>Next Question</button>
-        </div>
-    </div>
+        
+        <button onClick={(e) => submitAnswers(e)} className='next-button'>{countQuestion>=5? "Submit" : "Next Question"}</button> 
+        </div> ) : (<Result score={score} timer={timer}/>) }
+    </div>  
   )
 }
 
