fe1f4959cd4ac5f96a8145af886d51bc49062cdf
Anika Afroz <seikhirfan2011@gmail.com>
2022-11-26 19:28:08 +0000

    Getting questions from the backend

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -3,6 +3,7 @@ import Game from './components/Game';
 import Start from './components/Start';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Leaderboard from './components/Leaderboard';
+import Result from './components/Result';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" element={ <Start />}/>
           <Route exact path="/game" element={ <Game />}/>
           <Route exact path="/leaderboard" element={<Leaderboard />}/> 
+          <Route exact path="/result" element={<Result/>}/>
         </Routes>
       </BrowserRouter>
 
--- src/main/app/src/components/Leaderboard.js
+++ src/main/app/src/components/Leaderboard.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import './Leaderboard.css'
+import {useNavigate} from 'react-router-dom'
 
 const Leaderboard = () => {
+
+    const table = [];
+    for(let i = 1 ; i < 6 ; i++)
+        table.push(<div className='table'><h1 className='number'>{i}</h1><div className='row'><p>Tom</p><p>10</p></div></div>)
+
+
+        let navigate = useNavigate(); 
+        const routeChangeToHome = () =>{ 
+        let path = `/`; 
+        navigate(path);
+      }
+
+
   return (
     <div className='lead-container'>
         <h1 className='lead-title'>Leader Board</h1>
@@ -10,6 +24,8 @@ const Leaderboard = () => {
             <h2 className='nameH2'>Name</h2>
             <h2 className='scoreH2'>Score</h2>
         </div>
+        {table}
+        <img onClick={routeChangeToHome} className='homeLeadImg' src={require('./img/Home.png')} />
     </div>
   )
 }
