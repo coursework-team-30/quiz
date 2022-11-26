5a232712994cf6c3d1fbabfbe8816a80ccc7fa55
Muzammil <muzammilfive@gmail.com>
2022-11-26 01:38:00 +0000

    Created two APIs - leaderboard and score update [QA-7][QA-14]

--- src/main/java/com/uol/coursework/repository/LeaderBoardRepository.java
+++ src/main/java/com/uol/coursework/repository/LeaderBoardRepository.java
@@ -1,6 +1,15 @@
 package com.uol.coursework.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LeaderBoardRepository extends CrudRepository<LeaderBoard, Long> {
+import java.util.List;
+
+public interface LeaderBoardRepository extends CrudRepository<Leaderboard, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM leaderboard ORDER BY highest_score DESC LIMIT 10;")
+    List<Leaderboard> getTopScores();
+
+    @Query(nativeQuery = true, value = "select * from Leaderboard where user_name = ?1")
+    Leaderboard getRow(String userName);
 }
